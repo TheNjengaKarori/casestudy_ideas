440ab67d149b6b01cf3bd9be3dd2c97918f7c021
TheNjengaKarori <njenga.k11@gmail.com>
2023-11-26 17:38:02 +0100

    cosmetic changes

--- src/App.js
+++ src/App.js
@@ -4,47 +4,40 @@ import './App.css';
 const App = () => {
   const [currentIdea, setCurrentIdea] = useState(null);
   const [ideaContainerColorIndex, setIdeaContainerColorIndex] = useState(0);
-  const [usedIdeas, setUsedIdeas] = useState([]);
 
-  useEffect(() => {
+  const updateCurrentIdea = () => {
     fetch('/ideas.json')
       .then(response => response.json())
       .then(data => {
-        updateCurrentIdea(data);
+        const randomIndex = Math.floor(Math.random() * data.ideas.length);
+        setCurrentIdea(data.ideas[randomIndex]);
       })
       .catch(error => console.error('Error fetching ideas:', error));
-  }, []);
-
-  const updateCurrentIdea = (data) => {
-    const unusedIdeas = data.ideas.filter(idea => !usedIdeas.includes(idea.id));
-    if (unusedIdeas.length === 0) {
-      // All ideas have been used, reset usedIdeas array
-      setUsedIdeas([]);
-    }
-    const randomIndex = Math.floor(Math.random() * unusedIdeas.length);
-    const newIdea = unusedIdeas[randomIndex];
-    setCurrentIdea(newIdea);
-    setUsedIdeas([...usedIdeas, newIdea.id]);
-
-    // Set the next color in the predefined order, ensuring it's different from the current color
-    let nextColorIndex = (ideaContainerColorIndex + 1) % rainbowColors.length;
-    while (nextColorIndex === ideaContainerColorIndex) {
-      nextColorIndex = Math.floor(Math.random() * rainbowColors.length);
-    }
-    setIdeaContainerColorIndex(nextColorIndex);
   };
+
+  useEffect(() => {
+    updateCurrentIdea();
+  }, []); // Add updateCurrentIdea to the dependency array
 
   const handleNewChallenge = () => {
     fetch('/ideas.json')
       .then(response => response.json())
       .then(data => {
-        updateCurrentIdea(data);
+        const randomIndex = Math.floor(Math.random() * data.ideas.length);
+        setCurrentIdea(data.ideas[randomIndex]);
+
+        // Set the next color in the predefined order
+        const rainbowColors = ['#FF4500', '#8B4513', '#483D8B', '#006400', '#A52A2A', '#8B0000', '#2F4F4F', '#8B008B', '#8B008B', '#483C32','#1E90FF'];
+        const nextColorIndex = (ideaContainerColorIndex + 1) % rainbowColors.length;
+        setIdeaContainerColorIndex(nextColorIndex);
       })
       .catch(error => console.error('Error fetching ideas:', error));
   };
 
-  const rainbowColors = ['#FF4500', '#8B4513', '#483D8B', '#006400', '#A52A2A', '#8B0000', '#2F4F4F', '#8B008B', '#8B008B', '#483C32', '#1E90FF', '#8B0000', '#191970', '#2F4F4F', '#556B2F', '#008B8B', '#483D8B', '#8B008B', '#8B4513', '#B8860B', '#8FBC8F', '#9932CC', '#B22222'
-];
+  const getIdeaContainerColor = () => {
+    const rainbowColors = ['#FF4500', '#8B4513', '#483D8B', '#006400', '#A52A2A', '#8B0000', '#2F4F4F', '#8B008B', '#8B008B', '#483C32','#1E90FF',];
+    return rainbowColors[ideaContainerColorIndex];
+  };
 
   return (
     <div className="app-container">
@@ -55,9 +48,9 @@ const App = () => {
       </nav>
       <div className="main-content">
         <h1 className="main-heading">
-          Take the scenic route to UI/UX design greatness with 1000+ ideas for your case studies and personal projects.
+          Take the scenic route to UI/UX design greatness with 100+ ideas for your case studies and personal projects.
         </h1>
-        <div className="idea-container" style={{ backgroundColor: rainbowColors[ideaContainerColorIndex] }}>
+        <div className="idea-container" style={{ backgroundColor: getIdeaContainerColor() }}>
           {currentIdea && (
             <div className="idea-and-desc">
               <h2>{currentIdea.idea}</h2>
